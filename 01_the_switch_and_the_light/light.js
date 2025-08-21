c5caa303a5a91b69841dd4b861a104d6667b1637
David Rauschenbach <david@padi.io>
2025-08-21 12:22:49 -0700

    Read desired state changes from Arete events

--- 01_the_switch_and_the_light/light.js
+++ 01_the_switch_and_the_light/light.js
@@ -2,6 +2,7 @@ import { Gpio } from 'onoff';
 import { Client } from './cns.js';
 
 const GPIO23 = 535;
+const DESIRED_STATE_KEY = 'cns/network/nodes/sri4FZUq2V7S4ik2PrG4pj/contexts/kMqdHs8ZcskdkCvf1VpfSZ/provider/padi.button/connections/geizaJngWyA1AL3Nhn5dzD/properties/sState';
 
 // Configure pin for output
 let pin = new Gpio(GPIO23, 'out');
@@ -11,22 +12,20 @@ let client = new Client({protocol:'wss:', host:'dashboard.test.cns.dev', port:44
 await client.waitForOpen(5000);
 console.log('Connected to Arete control plane');
 
-// Read initial desired state
-let desired_state = Math.random() < 0.5; // TODO read from Arete
-console.log('Desired state is initially', desired_state ? 'ON' : 'OFF');
-
-// Actualize initial desired state
-pin.write(desired_state ? Gpio.HIGH : Gpio.LOW, err => {
-    if (err) {
-        throw err;
+// Detect future changes in desired state, and actualize it
+client.on('update', (event) => {
+    let value = event.keys[DESIRED_STATE_KEY];
+    if (!value) {
+        return;
     }
+    const desired_state = value == '1';
+    pin.writeSync(desired_state ? Gpio.HIGH : Gpio.LOW, err => {
+        if (err) {
+            throw err;
+        }
+    });
+    console.log('New desired state is', desired_state ? 'ON' : 'OFF');
 });
-
-// Startup complete
-console.log('Light service started')
-
-// Detect future changes in desired state
-setInterval(on_change, 1000); // TODO watch the Arete control plane instead of faking changes with a timer
 
 // Register shutdown handling
 process.on('SIGINT', _ => {
@@ -35,19 +34,5 @@ process.on('SIGINT', _ => {
     pin.unexport();
 });
 
-let last_desired_state = false; // TODO remove
-function on_change() {
-    let desired_state = Math.random() < 0.5; // TODO read from Arete
-    if (desired_state == last_desired_state) {
-        return;
-    }
-    console.log('New desired state is', desired_state ? 'ON' : 'OFF');
-
-    // Actualize new desired state
-    pin.writeSync(desired_state ? Gpio.HIGH : Gpio.LOW, err => {
-        if (err) {
-            throw err;
-        }
-    });
-    last_desired_state = desired_state;
-}
+// Startup complete
+console.log('Light service started')
