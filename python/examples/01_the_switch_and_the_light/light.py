eacdfbafe82b9c2fd37a68960b95074084913a3c
David Rauschenbach <david@padi.io>
2025-08-29 12:31:30 -0700

    Scaffold a light service

---
+++ python/examples/01_the_switch_and_the_light/light.py
@@ -0,0 +1,16 @@
+import sys
+
+# Configure pin for output
+# TODO
+
+# Connect to Arete control plane
+# TODO
+
+# Detect future changes in desired state, and actualize it
+# TODO
+
+# Register shutdown handling
+# TODO
+
+# Startup complete
+sys.stderr.write('Light service started\n')
