35502abeb64e5c57a5d8ff9062c9185fc6f9c46d
David Rauschenbach <david@padi.io>
2025-08-29 14:26:43 -0700

    Implement the light service hardware interface

--- python/examples/01_the_switch_and_the_light/switch.py
+++ python/examples/01_the_switch_and_the_light/switch.py
@@ -1,20 +1,36 @@
+import atexit
 import sys
+import time
+import RPi.GPIO as GPIO
+
+GPIO04 = 4
 
 # Configure pin for input
-# TODO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO04, GPIO.IN)
 
 # Connect to Arete control plane
 # TODO
 # sys.stderr.write('Connected to Arete control plane\n')
 
 # Read initial switch state, and sync it with Arete
-# TODO
+state = GPIO.input(GPIO04) == 0
+#client.put(DESIRED_STATE_KEY, state ? '1' : '0');
+sys.stderr.write('Switch is initially {}\n'.format('ON' if state else 'OFF'))
 
 # Detect future changes in switch state, and sync it with Arete
-# TODO
+def on_change(channel):
+    state = GPIO.input(GPIO04) == 0
+    #client.put(DESIRED_STATE_KEY, state ? '1' : '0');
+    sys.stderr.write('Switch is now {}\n'.format('ON' if state else 'OFF'))
+GPIO.add_event_detect(GPIO04, GPIO.BOTH, callback=on_change)
 
 # Register shutdown handling
-# TODO
+@atexit.register
+def cleanup():
+    GPIO.cleanup()
 
 # Startup complete
 sys.stderr.write('Switch service started\n')
+while True:
+    time.sleep(0.1)
