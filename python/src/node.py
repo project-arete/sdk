b2d37528cf5aa51f64ddf5311e55f55bf363610b
David Rauschenbach <david@padi.io>
2025-09-16 12:34:35 -0700

    fix

--- python/src/node.py
+++ python/src/node.py
@@ -7,7 +7,7 @@ class Node:
         self.id = id
 
     def context(self, id, name):
-        args = [self.system.id, id, id, name]
+        args = [self.system.id, self.id, id, name]
         transaction = self.client.send('json', 'contexts', args)
         self.client.wait_for_response(transaction)
         return Context(self, id)
