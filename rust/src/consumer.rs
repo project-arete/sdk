34164a07505777c49872bef5dc12f0b8b482f2ee
David Rauschenbach <david@padi.io>
2025-09-17 16:55:01 -0700

    cargo fmt

--- rust/src/consumer.rs
+++ rust/src/consumer.rs
@@ -1,8 +1,8 @@
-use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
+use super::{Client, Context, Error};
 use regex::Regex;
 use serde_json::Value;
-use super::{Client, Context, Error};
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
 
 pub struct Consumer {
     #[allow(unused)]
@@ -37,8 +37,7 @@ impl Consumer {
         let node_id = &self.context.node.id;
         let context_id = &self.context.id;
         let profile = &self.profile;
-        let key_prefix =
-            format!("cns/{system_id}/nodes/{node_id}/contexts/{context_id}/consumer/{profile}/");
+        let key_prefix = format!("cns/{system_id}/nodes/{node_id}/contexts/{context_id}/consumer/{profile}/");
         let upstream_rx = self.client.clone().on_update()?;
         let (tx, rx) = mpsc::channel();
         let re = Regex::new(r"connections/(\w+)/properties/(\w+)$")?;
@@ -52,7 +51,11 @@ impl Consumer {
                         let connection = captures[1].to_string();
                         let property = captures[2].to_string();
                         let value = v.clone();
-                        let change_event = ChangeEvent { connection, property, value };
+                        let change_event = ChangeEvent {
+                            connection,
+                            property,
+                            value,
+                        };
                         tx.send(change_event).unwrap();
                     };
                 }
@@ -60,5 +63,4 @@ impl Consumer {
         });
         Ok(rx)
     }
-
 }
