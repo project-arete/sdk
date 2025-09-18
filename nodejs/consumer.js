7a4be1762e2c1f07e26c5ba92d82639a62a03378
David Rauschenbach <david@padi.io>
2025-09-18 09:43:41 -0700

    (NodeJS) Example Light service obtains its connection key from the Arete control plane

--- nodejs/consumer.js
+++ nodejs/consumer.js
@@ -10,4 +10,28 @@ export class Consumer {
     this.context = context;
     this.profile = profile;
   }
+
+  watch(handler) {
+    const keyPrefix = `cns/${this.context.node.system.id}/nodes/${this.context.node.id}/contexts/${this.context.id}/consumer/${this.profile}/`;
+    const re = new RegExp(`connections/(\\w+)/properties/(\\w+)$`);
+    this.#client.on('update', (event) => {
+      for (let [key, value] of Object.entries(event.keys)) {
+        if (!key.startsWith(keyPrefix)) {
+          continue;
+        }
+        const captures = key.match(re);
+        if (captures.length < 3) {
+          continue;
+        }
+        const connection = captures[1];
+        const property = captures[2];
+        const changeEvent = {
+          connection,
+          property,
+          value,
+        };
+        handler(changeEvent);
+      }
+    });
+  }
 }
