0d8ae0a857dc34a5226d86fb3bb3b3490203f1ad
David Rauschenbach <david@padi.io>
2025-09-22 11:45:53 -0700

    (Python) Consumer and Provider classes/structs both require get, put, and watch capabilities

--- python/src/arete_sdk/provider.py
+++ python/src/arete_sdk/provider.py
@@ -1,13 +1,45 @@
+import re
+
+
 class Provider:
     def __init__(self, client, context, profile):
         self.client = client
         self.context = context
         self.profile = profile
 
-    def put(self, property, value):
+    def profile_key_prefix(self):
         system_id = self.context.node.system.id
         node_id = self.context.node.id
         context_id = self.context.id
         profile = self.profile
-        key = f'cns/{system_id}/nodes/{node_id}/contexts/{context_id}/provider/{profile}/properties/{property}'
+        return f'cns/{system_id}/nodes/{node_id}/contexts/{context_id}/provider/{profile}/'
+
+    def property_key(self, property):
+        profile_key_prefix = self.profile_key_prefix()
+        return f'{profile_key_prefix}properties/{property}'
+
+    def get(self, property):
+        key = self.property_key(property)
+        self.client.get(key)
+
+    def put(self, property, value):
+        key = self.property_key(property)
         self.client.put(key, value)
+
+    def watch(self, fn):
+        key_prefix = self.profile_key_prefix()
+
+        def on_update(event):
+            for key, value in event['keys'].items():
+                if not key.startswith(key_prefix):
+                    continue
+                captures = re.search('connections/(\\w+)/properties/(\\w+)$', key)
+                connection = captures.group(1)
+                property = captures.group(2)
+                change_event = {
+                    'connection': connection,
+                    'property': property,
+                    'value': value,
+                }
+                fn(change_event)
+        self.client.on_update(on_update)
